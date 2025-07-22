973238237be5e14af98fc565c509747350bcfeed
Kyle Guilfoyle <your.email@example.com>
2025-07-22 10:33:22 -0400

    Add Prisma migration for FunnelData model

---
+++ prisma/migrations/20250722143312_add_funnel_data/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "FunnelData" (
+    "id" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FunnelData_pkey" PRIMARY KEY ("id")
+);
